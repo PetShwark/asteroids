e67ad245b0ac59b4606d7abd1d247ec2d23e05ac
Peter Schwenk <paschwenk@gmail.com>
2025-12-03 12:35:39 -0500

    After "Splitting" lesson

--- asteroid.py
+++ asteroid.py
@@ -1,6 +1,8 @@
 import pygame
+import random
+from logger import log_event
 from circleshape import CircleShape
-from constants import LINE_WIDTH
+from constants import *
 
 class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
@@ -11,3 +13,22 @@ class Asteroid(CircleShape):
 
     def update(self, dt):
         self.position += self.velocity * dt
+
+    def split(self):
+        # Get rid of original asteroid
+        self.kill()
+        # Make two smaller asteroids to replace it
+        if self.radius > ASTEROID_MIN_RADIUS:
+            log_event("asteroid_split")
+            toggle = True
+            for _ in range(ASTEROID_SPLIT_COUNT):
+                if toggle:
+                    direction = 1
+                    toggle = False
+                else:
+                    direction = -1
+                    toggle = True
+                new_radius = self.radius - ASTEROID_MIN_RADIUS
+                new_asteroid = Asteroid(self.position.x, self.position.y, new_radius)
+                new_velocity = self.velocity.rotate(direction*random.uniform(20, 50))*ASTEROID_SPEEDUP_FACTOR
+                new_asteroid.velocity = new_velocity
